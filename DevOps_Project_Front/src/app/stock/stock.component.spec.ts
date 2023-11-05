c799e480770a85f16980d1fe53de7b08e4838ed5
mouhamed messaoudi <98428544+mouhamed369@users.noreply.github.com>
2023-11-05 02:49:40 -1200

    finished spring boot tests

--- DevOps_Project_Front/src/app/stock/stock.component.spec.ts
+++ DevOps_Project_Front/src/app/stock/stock.component.spec.ts
@@ -1,21 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { StockComponent } from './stock.component';
+import { StockService } from '../services/stock.service';
+import { FormsModule } from '@angular/forms';
 
 describe('StockComponent', () => {
   let component: StockComponent;
   let fixture: ComponentFixture<StockComponent>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
+      providers: [StockService],
       declarations: [StockComponent]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(StockComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(StockComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call onButtonClick when clicked', fakeAsync(() => {
+    spyOn(component, 'showForm');
+
+    let showFormBtn = fixture.debugElement.nativeElement.querySelector('#showForm');
+    let inputlabel = fixture.debugElement.nativeElement.querySelector("#input-label");
+    showFormBtn.click();
+
+    tick();
+
+    expect(component.showForm()).toHaveBeenCalled();
+    expect(inputlabel).toBeTruthy();
+  }));
+
+
 });
